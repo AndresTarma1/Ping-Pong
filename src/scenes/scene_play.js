88f89af94eea82c2b5ef01495974fcdf61de7ae1
tarma <tarraztamara543@gmail.com>
2024-05-30 21:28:42 -0500

    Juego completo

--- src/scenes/scene_play.js
+++ src/scenes/scene_play.js
@@ -25,9 +25,44 @@ class Scene_play extends Phaser.Scene {
         //fisicas
         this.physics.add.collider(this.ball, this.izquierda,this.chocaPala , null, this);
         this.physics.add.collider(this.ball, this.derecha,this.chocaPala , null, this);
+    
+        //Controles
+        //Pala derecha
+        this.cursor = this.input.keyboard.createCursorKeys();
+
+        //Pala izquierda
+        this.cursor_W = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        this.cursor_S = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+
     }
 
     chocaPala(){
+        this.ball.setVelocityY(Phaser.Math.Between(-120, 120));
+    }
+
+    update(){
+        if(this.ball.x < 0 || this.ball.x > this.sys.game.config.width){
+            this.ball.setPosition(this.sys.game.config.width/2,this.sys.game.config.height/2);
+        }
+
+        //Control de las palas
+        //Pala derecha
+        if(this.cursor.down.isDown){
+            this.derecha.body.setVelocityY(300);
+        }else if(this.cursor.up.isDown){
+            this.derecha.body.setVelocityY(-300);
+        }else{
+            this.derecha.body.setVelocityY(0);
+        }
+
+        //Pala izquierda
+        if(this.cursor_W.isDown){
+            this.izquierda.body.setVelocityY(-300);   
+        }else if(this.cursor_S.isDown){
+            this.izquierda.body.setVelocityY(300);   
+        }else{
+            this.izquierda.body.setVelocityY(0);
+        }
 
     }
 }
